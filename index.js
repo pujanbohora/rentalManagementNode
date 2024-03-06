ed0f80d4e31267d0bc583c4edaa0c75e10d4c093
Pujan Bohora <bohorapawan@gmail.com>
2024-03-06 19:58:52 +0545

    change

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
+app.get("/test", (req, res) => {
+  res.send('YEST THIS IS WORKING')
+});
+
+
 app.listen(parseInt(process.env.PORT), () => {
   console.log(`Server is running at port ${process.env.PORT}`);
 });
